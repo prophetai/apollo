f621a5d8e0d5c8050610bc6a90c9e7a2044749cd
Esteban Diaz <estebandiaz@MacBook-Pro-mrdeds.local>
2019-06-16 23:49:42 -0500

    Trading hours, profit > loss

--- apollo/src/trade/pip_calculator.py
+++ apollo/src/trade/pip_calculator.py
@@ -2,32 +2,27 @@
 
 def get_profit(open_price, tp_price, lots):
         pip_dif = abs(tp_price - open_price)
-
         profit_yen = pip_dif * lots * 1000
-
         profit_dlls = profit_yen / tp_price
 
         return profit_dlls
+
 def get_loss(open_price, sl_price, lots):
     pip_dif = sl_price - open_price
-
     loss_yen = pip_dif * lots * 1000
-
     loss_dlls = loss_yen / sl_price
 
     return loss_dlls
 
 def get_loss_sell(open_price, sl_price, lots):
     pip_dif = sl_price - open_price
-
     loss_yen = pip_dif * lots * 1000
-
     loss_dlls = loss_yen / sl_price
 
     return loss_dlls
 
 if __name__ == '__main__':
     open_price = 109.562
-    close_price = 109.678
+    close_price = 109.662
     lots = 1
     print(get_profit(open_price, close_price, lots))
