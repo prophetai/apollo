dd2c923db53f78c57a8d342a8e153c0428fd32d4
Dudley Esteban Díaz Suárez <deds15@gmail.com>
2020-01-08 11:59:54 -0600

    cambio de probabilidad base de .6 a .7

--- apollo/src/trade/logic.py
+++ apollo/src/trade/logic.py
@@ -53,11 +53,11 @@ class Decide:
             spread = self.spread * pips * 10  #checar la lógica/razón de esto
             probability_cand = data.iloc[i]['Probability']
             profit_cand = get_profit(data.iloc[i]['Open'], data.iloc[i]["Take Profit"], pips)
-            # Si la proba es > 0.6, y la ganancia cubre al menos el spread, entonces ese utilizamos
-            if probability_cand >= 0.6:
+            # Si la proba es > 0.7, y la ganancia cubre al menos el spread, entonces ese utilizamos
+            if probability_cand >= 0.7:
                 print('\nNuevo candidato:')
                 print(f'Best_action:\n{data.iloc[i]}')
-                if probability_cand >= 0.7:
+                if probability_cand >= 0.8:
                     profit_cand *= 2
                     spread *= 2
                 if profit_cand/spread >= 1.6 and profit_cand > max_profit:
