fc108478b948566893ed07ed8553e9ab753ad054
Dudley Esteban Díaz Suárez <deds15@gmail.com>
2020-12-14 20:09:59 -0600

    Proba desde DB y duración Se obtiene probabilidad desde BD y se calcula duración de trade

--- src/saveToDB.py
+++ src/saveToDB.py
@@ -3,14 +3,25 @@ import logging
 import pandas as pd
 import numpy as np
 import oandapy as opy
-import trade
+from trade import trade
 from datetime import datetime as dt
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from loadAssets import Assets
 import json
 
-def get_engine(conn_data):
+with open("src/settings.py", "r") as file:
+    exec(file.read())
+conn_data = {
+    'db_user': os.environ['POSTGRES_USER'],
+    'db_pwd': os.environ['POSTGRES_PASSWORD'],
+    'db_host': os.environ['POSTGRES_HOST'],
+    'db_name': os.environ['db_name'],
+    'db_port': os.environ['db_port']
+}
+
+
+def __get_engine():
     """
     Gets a connection engine for a database
 
@@ -21,13 +32,15 @@ def get_engine(conn_data):
     pwd = conn_data['db_pwd']
     host = conn_data['db_host']
     data_base = conn_data['db_name']
+    port = conn_data['db_port']
 
-    engine = create_engine(f'postgresql://{user}:{pwd}@{host}:5432/{data_base}')
+    engine = create_engine(
+        f'postgresql://{user}:{pwd}@{host}:{port}/{data_base}')
 
     return engine
 
 
-def save_order(account,model,instrument,order,probability,conn_data):
+def save_order(account, model, instrument, order, probability):
     """
     Saves the desition made from the predictions
 
@@ -37,93 +50,90 @@ def save_order(account,model,instrument,order,probability,conn_data):
         - instrument(str): instrument traded
         - order(Order): order data from broker
         - probability(float): probability from decision
-        - conn_data(dic): dictionary to connect to the database
-            conn_data = {
-            'db_user': ,
-            'db_pwd':  ,
-            'db_host': ,
-            'db_name': 
-    }
     """
-    if order.trade.units > 0: # We are making a Buy
+    if order.trade.units > 0:  # We are making a Buy
         trade = "buy"
-    elif order.trade.units < 0: # We are making a Sell
-        trade = "sell"   
-    else: # We are not making a trade
+    elif order.trade.units < 0:  # We are making a Sell
+        trade = "sell"
+    else:  # We are not making a trade
         logging.info('Neutral - No decision to save')
         return
 
     account_type = "live"
-    trading_url = os.environ['trading_url_'+ account]
+    trading_url = os.environ['trading_url_' + account]
     account = trading_url.split('/')[-2]
-    
+
     if 'practice' in trading_url:
         account_type = "practice"
 
-    engine = get_engine(conn_data)
-    
-    data = pd.DataFrame(data = {"account": account,
-                            "order_id": [order.i_d],
-                            "account_type": [account_type],
-                            "entry_price":[order.entry_price],
-                            "ask": [order.ask_price],
-                            "bid": [order.bid_price],
-                            "instrument": [order.trade.instrument],
-                            "model": [model],
-                            "units": [order.trade.units],
-                            "probability": [probability],
-                            "stop_loss": [order.trade.stop_loss],
-                            "take_profit": [float(order.trade.take_profit)],
-                            "time": [dt.now()],
-                            "trade": [trade]})
+    engine = __get_engine()
+
+    data = pd.DataFrame(data={"account": account,
+                              "order_id": [order.i_d],
+                              "account_type": [account_type],
+                              "entry_price": [order.entry_price],
+                              "ask": [order.ask_price],
+                              "bid": [order.bid_price],
+                              "instrument": [order.trade.instrument],
+                              "model": [model],
+                              "units": [order.trade.units],
+                              "probability": [probability],
+                              "stop_loss": [order.trade.stop_loss],
+                              "take_profit": [float(order.trade.take_profit)],
+                              "time": [dt.now()],
+                              "trade": [trade]})
     logging.info('Data to save on Database')
     logging.info(data.reset_index(drop=True).to_dict())
     data = data.reset_index(drop=True)
     try:
-        data.to_sql('trades', engine, if_exists="append",index=False)
+        data.to_sql('trades', engine, if_exists="append", index=False)
     except Exception as e:
         logging.error(e)
-    
-def save_input(account, model_version, current_time, inv_instrument, original_datasets, conn_data, order_id=None):
+
+
+def save_input(account, model_version, current_time, inv_instrument, original_datasets, order_id=None):
     """
     Saves model input data to Database
 
     -
-    """        
+    """
     assets = Assets(model_version, inv_instrument)
     variablesh, variablesl = assets.load_vals()
 
-    data_high = {variablesh[i]: original_datasets[0][0][i] for i in range(len(original_datasets[0][0]))}
-    data_low = {variablesl[i]: original_datasets[1][0][i] for i in range(len(original_datasets[1][0]))}
-    
+    data_high = {variablesh[i]: original_datasets[0][0][i]
+                 for i in range(len(original_datasets[0][0]))}
+    data_low = {variablesl[i]: original_datasets[1][0][i]
+                for i in range(len(original_datasets[1][0]))}
+
     df_todb = pd.DataFrame()
-    df_todb['account'] = [os.environ['trading_url_'+ account].split('/')[-2]]
+    df_todb['account'] = [os.environ['trading_url_' + account].split('/')[-2]]
     df_todb['model_version'] = [model_version]
     df_todb['date'] = [current_time]
     df_todb['order_id'] = [order_id]
     df_todb['data_high'] = [json.dumps(data_high)]
     df_todb['data_low'] = [json.dumps(data_low)]
-    
-    
-    engine = get_engine(conn_data)
+
+    engine = __get_engine()
     try:
         df_todb.to_sql(f'historical_datasets',
-            engine, if_exists="append", index=False)
+                       engine, if_exists="append", index=False)
     except Exception as e:
         logging.error(e)
 
 
-def get_trade_from_id(trade_id, conn_data):
+def get_trade_from_id(trade_id):
     """
     Gets a trade from the DB using its ID
-    """ 
-    engine = get_engine(conn_data)
-
+    """
+    engine = __get_engine()
     try:
-        session = sessionmaker(bind=engine)
-        trade_data = session.query(Trade).filter_by(id=trade_id).first() 
+        with engine.connect() as con:
+            result = con.execute(
+                f"SELECT * FROM public.trades where order_id = \'{trade_id}\';")
     except Exception as e:
         logging.error(e)
+    result = result.fetchone()
 
-    return trade_data
-#def save_stop_loss()
+    return result
+
+# TODO #102 def save_stop_loss()
--- src/trade/trade.py
+++ src/trade/trade.py
@@ -1,9 +1,12 @@
 import os
 import logging
 import requests
-from sqlalchemy import orm
+import math
+from sqlalchemy import orm, Column, String, Integer, Numeric, DateTime
 from saveToDB import get_trade_from_id
 from datetime import datetime as dt
+from sqlalchemy.ext.declarative import declarative_base
+Base = declarative_base()
 
 logging.basicConfig(
     format='%(asctime)s - %(levelname)s - %(message)s',
@@ -11,15 +14,18 @@ logging.basicConfig(
     level=logging.INFO
 )
 
-class Trade():
-    __table_name__ = 'trades'
+
+class Trade(Base):
+    __tablename__ = 'trades'
+    id = Column(Integer, primary_key=True)
+
     def __init__(self, instrument, units, i_d=None, price=None, take_profit=None, openTime=None):
         """Inicializa un trade
         Args:
             instrument (str): investment instrument name
             max_units (float): maximum quantity of units that can be traded at the same time
         """
-        
+        self.__table_name__ = 'trades'
         self.i_d = i_d
         self.instrument = instrument
         self.units = units
@@ -29,13 +35,13 @@ class Trade():
         self.openTime = openTime
 
     def __repr__(self):
+
         return f"<Trade(id={self.i_d},\
                 instrument = {self.instrument},\
                 stop_loss = {self.stop_loss},\
                 take_profit = {self.take_profit},\
                 units = {self.units}>"
-    
-    
+
     def get_stop_loss(self):
         """
         Gets stop loss for current trade
@@ -45,9 +51,17 @@ class Trade():
                           ((1-probability)*self.units *0.01)      
         """
         probability = self.__get_probability()
-        take_profit_pips = abs(self.take_profit - self.price)
-        self.stop_loss = (((self.units * (take_profit_pips * 0.01/self.take_profit)) * probability * self.price)/ ((1-probability)*self.units *0.01))
-    
+        take_profit = float(self.take_profit)
+        price = float(self.price)
+        units = float(self.units)
+        take_profit_pips = abs(take_profit - price)
+
+        inv_direction = price - take_profit # gets the sign of the SL (inverted)
+        calc_sl = (((units * (take_profit_pips * 0.01/take_profit)) * probability * price) / ((1-probability) * units * 0.01))
+        
+        self.stop_loss = round(price + math.copysign(calc_sl, inv_direction),3)
+        print(f"SL calculated for trade ({self.i_d}): {self.stop_loss}")
+
     def get_trade_duration(self):
         """
         Gets how long the trade has been open in hours.
@@ -58,21 +72,22 @@ class Trade():
         hours = abs(divmod(duration_in_s, 3600)[0])
 
         return hours
-    
+
     def __get_probability(self):
         """
         Gets the probability of success from the trade's DB
         """
         data = get_trade_from_id(self.i_d)
-        probability = data['probability']
+        print(f'data:{data}')
+        probability = round(float(data.probability), 2)
 
         return probability
 
 
-        
 if __name__ == "__main__":
-    trade = Trade(1,'USD_JPY',110.101, 2000, 111.00)
+    trade = Trade(1, 'USD_JPY', 110.101, 2000, 111.00)
     probability = 0.7
     trade.get_stop_loss(probability)
 
-    logging.info(f'ID:{trade.i_d} \nUnits:{trade.units} \nStop Loss:{trade.stop_loss}')
+    logging.info(
+        f'ID:{trade.i_d} \nUnits:{trade.units} \nStop Loss:{trade.stop_loss}')
